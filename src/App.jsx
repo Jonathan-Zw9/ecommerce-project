aae208e5d370cb6616e6f71a1739c06b4f7f6dc7
Jonathan-Zw9 <matamburaj091@gmail.com>
2026-09-26 10:03:02 +0200

    Updated checkout components and moved all of them into the checkout folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { Routes, Route } from 'react-router'
-import { CheckoutPage } from './pages/CheckoutPage'
+import { CheckoutPage } from './pages/checkout/CheckoutPage'
 import { HomePage } from './pages/HomePage'
 import { OrdersPage } from './pages/OrdersPage'
 import { TrackingPage } from './pages/TrackingPage'
